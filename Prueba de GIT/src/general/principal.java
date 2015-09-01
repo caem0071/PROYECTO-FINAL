6b42f56f75ec229c99575897145304fec1f7271e
Cristhian Espinosa <caem0071@hotmail.com>
2015-08-31 23:55:50 -0500

    Proyecto en netbeans

---
+++ Prueba de GIT/src/general/principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package general;
+
+import static java.sql.DriverManager.println;
+
+/**
+ *
+ * @author High PC Solutions
+ */
+public class principal {
+    public static void main(String args[]){
+        println("HOLA MUNDO GIT");
+    }
+    
+}
